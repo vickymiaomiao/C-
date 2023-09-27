def30cc25d8bd9b887ec5b9048bd57044881ded8
vickymiaomiao <Vickme@163.com>
2023-09-27 09:52:11 +0800

    读二进制文件

--- 21_02二进制文件读写.cpp
+++ 21_02二进制文件读写.cpp
@@ -8,6 +8,9 @@ using namespace std;
 * 二进制方式写文件主要利用刘对象调用成员函数write
 * 函数原型：ostream& write(const char * buffer,int len);
 * 参数解释：字符指针buffer指向内存中一段存储空间，len是读写的字节数
+* 
+* 读文件
+* 函数原型： istream& read(char *buffer,int len)
 */
 
 //二进制写文件
@@ -29,8 +32,26 @@ void test01()
 
 //读文件
 
+void test02()
+{
+	ifstream ifs;
+	ifs.open("person.txt", ios::in | ios::binary);
+	if (!ifs.is_open())
+	{
+		cout << "open fail" << endl;
+		return;
+	}
+	Person p;
+	ifs.read((char*)&p, sizeof(Person));
+	cout << p.m_Name << "  " << p.m_Age << endl;
+	ifs.close();
+
+}
+
+
 int main()
 {
 	test01();
+	test02();
 	return 0;
 }
