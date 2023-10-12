7765bf612810fcbc4de221a73805bda57fbb8b0c
vickymiaomiao <Vickme@163.com>
2023-10-12 18:07:45 +0800

    list容器赋值、交换、大小、插入和删除操作

--- 提高编程/10 list容器.cpp
+++ 提高编程/10 list容器.cpp
@@ -1,1 +1,155 @@
 #define _CRT_SECURE_NO_WARNINGS 1   
+#include <list>
+#include <iostream>
+using namespace std;
+
+/*
+* 链表：将数据进行链式存储
+* 链表是一种物理存储单元上非连续的存储结构，数据元素的逻辑顺序是通过链表中的指针链接实现的
+* 
+* 链表由一系列结点组成
+* 结点的组成：一个是存储数据元素的数据域，另一个是存储下一个结点地址的指针域
+* 优点，可以对任意位置进行快速插入或者删除元素，采用动态存储分配，不会造成内存浪费
+* 缺点：容器遍历速度没有数组快，占用空间比数组大
+* 
+* 双向循环链表，每个结点有两个指针（指向下一个和上一个）
+* 链表的迭代器是双向迭代器，只支持前移和后移
+* 
+* list插入和删除操作都不会导致原有的list迭代器的失效，这在vector是不成立的
+* vector和list是两个最常使用的容器
+*/
+
+//遍历容器
+void printList(const list<int>& l)
+{
+	for (list<int>::const_iterator it = l.begin(); it != l.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+//构造函数
+void test01()
+{
+	list<int>l1;
+	//添加数据
+	l1.push_back(10);
+	l1.push_back(10); 
+	l1.push_back(10); 
+	l1.push_back(10);
+	//遍历容器
+	printList(l1);
+
+	//按照区间
+	list<int>l2(l1.begin(),l1.end());
+	printList(l2);
+	//拷贝构造
+	list<int>l3(l1);
+	printList(l3);
+	//N个elem
+	list<int>l4(10, 100);
+	printList(l4);
+}
+
+//赋值和交换  assign = swap
+void test02()
+{
+	list<int>l1;
+	//添加数据
+	l1.push_back(10);
+	l1.push_back(20);
+	l1.push_back(30);
+	l1.push_back(40);
+	printList(l1);
+	list<int>l2;
+	l2 = l1;
+	printList(l2);
+	list<int>l3;
+	l3.assign(l2.begin(), l2.end());
+	printList(l3);
+	list<int>l4;
+	l4.assign(10,100);
+	printList(l4);
+
+	//swap
+	l4.swap(l1);
+	cout << "after swap" << endl;
+	printList(l1);
+	printList(l4);
+
+}
+
+//大小  empty size resize
+void test03()
+{
+	list<int>l1;
+	l1.push_back(10);
+	l1.push_back(20);
+	l1.push_back(30);
+	l1.push_back(40);
+	printList(l1);
+	//判断容器是否为空
+	if (l1.empty())
+	{
+		cout << "l1 is empty" << endl;
+	}
+	else
+	{
+		cout << "l1 is not empty" << endl;
+		cout << "l1 size is : " << l1.size() << endl;
+	}
+	//重新指定大小
+	l1.resize(6);
+	printList(l1);
+	l1.resize(10,100);
+	printList(l1);
+	l1.resize(4);
+	printList(l1);
+}
+
+//插入和删除
+void test04()
+{
+	list<int>L;
+	//尾插
+	L.push_back(10);
+	L.push_back(20);
+	L.push_back(30);
+	//头插
+	L.push_front(100);
+	L.push_front(200);
+	L.push_front(300);
+	printList(L);
+	//尾删
+	L.pop_back();
+	//头删
+	L.pop_front();
+	printList(L);
+	//插入
+	list<int>::iterator it = L.begin();
+	L.insert(++it, 1000);
+	printList(L);
+	//删除  clear清空
+	it = L.begin();
+	L.erase(it);
+	printList(L);
+	//remove移除  容器中所有的匹配元素
+	L.push_back(10000);
+	printList(L);
+	L.remove(10000);
+	printList(L);
+	//清空
+	L.clear();
+	printList(L);
+}
+
+
+int main()
+{
+	//test01();
+	//test02();
+	//test03();
+	test04();
+	return 0;
+}
